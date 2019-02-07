a1167deaa5bdef058245f7f274e73e982d24d606
usuarioDgithub <47405531+usuarioDgithub@users.noreply.github.com>
2019-02-06 18:09:06 -0800

    version 2
    aaaaaaaaaaaaaaaa

--- src/paquete/test/test.java
+++ src/paquete/test/test.java
@@ -7,6 +7,7 @@ public class test {
 		
 		paquetes.persona p = new paquetes.persona(21, "Leo","Masculino");
 		paquetes.subpaquete.persona p2 = new paquetes.subpaquete.persona(21, "Alan");
+		System.out.println("inicio de impresion.");
 		System.out.println(p.toString());
 		System.out.println(p2.toString());
 		
